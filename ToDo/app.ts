cda49b567ae033dc7fe89f154b4e8d6a87a78bc1
Josip Maras <jomaras@gmail.com>
2015-11-18 15:53:11 +0100

    Minor updates, new projects

---
+++ ToDo/app.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="typings/angular2/angular2.d.ts" />
+import { Component, View, bootstrap } from "node_modules/angular2/angular2";
+
+@Component({
+    selector:""
+})
+
+class MyComponent {
+    name: string;
+    age: number;
+    isEnabled: boolean;
+}
